0f619aa1182701351a06609730c9fbfcecb606a4
Yana Sirina <yanasirina0@yandex.ru>
2022-11-02 20:10:11 +0500

    Добавила поиск по имени и автору (/?search=...)

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from . import models
@@ -8,5 +9,6 @@ from . import serializers
 class BookViewSet(ModelViewSet):
     queryset = models.Book.objects.all()
     serializer_class = serializers.BookSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['price']
+    search_fields = ['name', 'author_name']
