694cf8b8511b6b71752f86b4fed025f4f335bd5d
Yana Sirina <yanasirina0@yandex.ru>
2022-11-02 19:37:09 +0500

    Подключила django-filter

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 
 from . import models
@@ -7,3 +8,5 @@ from . import serializers
 class BookViewSet(ModelViewSet):
     queryset = models.Book.objects.all()
     serializer_class = serializers.BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['price']
