88fa56324c5b6bbf7a4bba98b64f1290deafa361
Yana Sirina <yanasirina0@yandex.ru>
2022-11-23 22:09:20 +0500

    незначительные изменения

--- store/views.py
+++ store/views.py
@@ -12,4 +12,4 @@ class BookViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['price']
     search_fields = ['name', 'author_name']
-    ordering_fields = ['price', 'name', 'author_name']
+    ordering_fields = ['price']
