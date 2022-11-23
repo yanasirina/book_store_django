e4dc1349af678f7d6477a10cd90f5e0256cff58d
Yana Sirina <yanasirina0@yandex.ru>
2022-11-23 22:19:14 +0500

    подправила тесты

--- store/tests/test_serializers.py
+++ store/tests/test_serializers.py
@@ -10,17 +10,20 @@ class BookSerializerTestCase(TestCase):
         book2 = Book.objects.create(name='Test Book 2', price=10)
 
         serializer_data = BookSerializer([book1, book2], many=True).data
+        formatted_serializer_data = [dict(element) for element in serializer_data]
         expected_data = [
             {
                 'id': book1.id,
                 'name': 'Test Book 1',
-                'price': '100.10'
+                'price': '100.10',
+                'author_name': None
             },
             {
                 'id': book2.id,
                 'name': 'Test Book 2',
-                'price': '10.00'
+                'price': '10.00',
+                'author_name': None
             }
         ]
 
-        self.assertEqual(expected_data, serializer_data)
+        self.assertEqual(expected_data, formatted_serializer_data)
